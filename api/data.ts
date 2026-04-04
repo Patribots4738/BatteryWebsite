2326f12ac9c633f3bca38646cac4c7fa9cc63a92
fsk <surfluke19@gmail.com>
2026-04-04 13:53:35 -0700

    firebase more worky now

--- api/data.ts
+++ api/data.ts
@@ -5,14 +5,14 @@ export async function parseData(data: JsonData) {
 	const fromRemote: JsonData = JSON.parse(JSON.stringify(data)) as JsonData;
 	const batteryNumber: number = fromRemote.batteryNumber;
 	const dateString: string = `${fromRemote.header.date.month}-${fromRemote.header.date.day}-${fromRemote.header.date.year}`;
-	const timeString: string = `${fromRemote.header.time.hour}:${fromRemote.header.time.minute}:${fromRemote.header.time.second}`;
+	const timeString: string = `${fromRemote.header.time.hour}-${fromRemote.header.time.minute}-${fromRemote.header.time.second}`;
 	const fullDateTimeString: string = `${dateString}_${timeString}`;
 	
-	const latestBatteryData = ref(db, `/num/${batteryNumber}/latest`);
-	const headerDb = ref(db, `/num/${batteryNumber}/headers`);
+	const latestBatteryData = ref(db, `/num/${batteryNumber}/latest/`);
+	const headerDb = ref(db, `/num/${batteryNumber}/headers/`);
 	const fullDataDb = ref(db, `/allData/${batteryNumber}/${fullDateTimeString}`);
-	const recentlyUsedList = ref(db, `/recentlyUsed`);
-	const lastChangedBattery = ref(db, '/latest');
+	const recentlyUsedList = ref(db, `/recentlyUsed/`);
+	const lastChangedBattery = ref(db, '/latest/');
 	const lastChangedBatteryData = (await get(lastChangedBattery)).toJSON() as JsonData | null;
 	
 	if (lastChangedBatteryData?.header.movingTo.search(/Charger/) !== -1) {
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
+import { StrictMode } from 'react';
+import './index.css';
+import App from './App';
+import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const element: HTMLElement | null = document.getElementById('root');
+if (!element) {
+    throw new Error('Root element not found');
+}
+
+const root = createRoot(element)
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
 )
