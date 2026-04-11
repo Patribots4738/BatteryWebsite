e1f243d8b2217c6b78e62bab2ec0ad7b5e668fc0
BredPerson <155865812+BredPerson@users.noreply.github.com>
2026-04-10 17:59:54 -0700

    Omg async can go die I hate it also latest used is done now 🫃

--- src/PromisedLand.tsx
+++ src/PromisedLand.tsx
@@ -1,27 +1,8 @@
-//import { useState } from 'react'
-//import { getDataFromFirebase } from '../shared/data';
-/*
-import { 
-	BatteryNames,
-	type JsonData,
-	type Header 
-} from '../shared/types'
-*/
+import { getDataFromFirebase } from '../shared/data';
+import { type JsonData } from '../shared/types';
 
 // Data Retrival
-/*
-async function getLatestUsed() {
-	let fireData = await getDataFromFirebase('/recentlyUsed') as object
-	for (const data in fireData) {
-		console.log(data);
-	}
-}
 
-async function getNumData() {
-	const data = await getDataFromFirebase('/num/');
+export async function getLatestUsed() {
+	return (await getDataFromFirebase('/recentlyUsed')) as JsonData[];
 }
-*/
-//const [allData, setAllData] = useState('/allData/')
-//const [latetBattery, setLatestBattery] = useState('/latest/')
-//const [numData, setNumData] = useState('/num/')
-//const [recentBattery, setRecentBattery] = useState('/recentlyUsed/')
